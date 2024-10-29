f989b92dcbde923541504a65e2ec536f5179feb2
Tuva <tuva.tma@gmail.com>
2024-10-29 23:52:20 +0100

    forsøk på fix

--- portofolio/frontend/src/features/projects/services/projectsApi.tsx
+++ portofolio/frontend/src/features/projects/services/projectsApi.tsx
@@ -41,3 +41,70 @@ const list = async (): Promise<{
 };
 
 export default { list };
+
+/*
+import {
+  validateCreateProject,
+  type CreateProject,
+  type Project,
+  type ProjectResponse,
+  type UpdateProject,
+} from "../../../../../backend/src/features/projects/helpers";
+
+export const createProjectService = (projectRepository: ProjectRepository) => {
+  const getById = async (id: string): Promise<Result<Project | undefined>> => {
+    return projectRepository.getById(id);
+  };
+
+  const list = async (query?: Query): Promise<Result<ProjectResponse[]>> => {
+    const result = await projectRepository.list(query);
+    if (!result.success) return result;
+
+    return {
+      ...result,
+      data: result.data.map(createProjectResponse),
+    };
+  };
+
+  const create = async (data: CreateProject): Promise<Result<string>> => {
+    const project = createProject(data);
+
+    if (!validateCreateProject(project).success) {
+      return {
+        success: false,
+        error: { code: "BAD_REQUEST", message: "Invalid project data" },
+      };
+    }
+    return projectRepository.create(project);
+  };
+
+  const update = async (data: UpdateProject) => {
+    const project = createProject(data);
+
+    if (!validateCreateProject(project).success) {
+      return {
+        success: false,
+        error: { code: "BAD_REQUEST", message: "Invalid project data" },
+      };
+    }
+
+    return projectRepository.update(project);
+  };
+
+  const remove = async (id: string) => {
+    return projectRepository.remove(id);
+  };
+
+  return {
+    list,
+    create,
+    update,
+    getById,
+    remove,
+  };
+};
+
+export const projectService = createProjectService(projectRepository);
+
+export type ProjectService = ReturnType<typeof createProjectService>;
+*/
